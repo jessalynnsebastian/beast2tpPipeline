bdb3d43a28d1edfc0ed482713980a9a73f6e3138
Jessalyn Sebastian <jessalys@uci.edu>
2024-12-13 16:44:41 -0800

    updates to vignette + added to example folder

--- R/run_TransPhylo.R
+++ R/run_TransPhylo.R
@@ -28,35 +28,8 @@
 #' the TransPhylo results will be saved.
 #' @param output_name A character string specifying the name of the output
 #' file.
-#' @param gentime_shape A numeric specifying the shape parameter of the gamma
-#' distribution for the generation time.
-#' @param gentime_scale A numeric specifying the scale parameter of the gamma
-#' distribution for the generation time.
-#' @param sampling_shape A numeric specifying the shape parameter of the gamma
-#' distribution for the sampling time. Default is the same as `gentime_shape`.
-#' @param sampling_scale A numeric specifying the scale parameter of the gamma
-#' distribution for the sampling time. Default is the same as `gentime_scale`.
-#' @param prior_sampfrac_a A numeric specifying the first shape parameter of the beta
-#' distribution for the sampling fraction.
-#' @param prior_sampfrac_b A numeric specifying the second shape parameter of the beta
-#' distribution for the sampling fraction.
-#' @param start_off_p A numeric specifying the starting value for the off.p parameter.
-#' @param start_neg A numeric specifying the starting value for the neg parameter.
-#' @param start_pi A numeric specifying the starting value for the pi parameter.
-#' @param start_off_r A numeric specifying the starting value for the off.r parameter.
-#' @param share A character vector specifying which parameters to share across
-#' clusters. Defaults to "neg" and "off.r". If `type` is "trees_sample", this
-#' parameter is ignored.
-#' @param update_neg A logical specifying whether to update the neg parameter.
-#' @param update_off_r A logical specifying whether to update the off.r parameter.
-#' @param update_off_p A logical specifying whether to update the off.p parameter.
-#' @param update_pi A logical specifying whether to update the pi parameter.
-#' @param optiStart A numeric specifying type of optimization to apply to
-#' MCMC start point.
-#' @param verbose A logical specifying whether to print verbose output.
-#' @param delta_t The grid size for the MCMC. Smaller is better but slower.
-#' @param mcmc_iterations A numeric specifying the number of MCMC iterations.
-#' @param thinning A numeric specifying the thinning parameter for the MCMC.
+#' @param ... Additional arguments to be passed to
+#' `TransPhylo::infer_multittree_share_param`.
 #'
 #' @returns A data frame containing the probability of each sample being an
 #' infection source.
@@ -73,28 +46,15 @@ run_TransPhylo <- function(trees,
                            cluster_dict,
                            out_dir = "TransPhylo",
                            output_name = "tp_res",
-                           gentime_shape = 10,
-                           gentime_scale = 1 / 10,
-                           sampling_shape = 10,
-                           sampling_scale = 1/10,
-                           prior_sampfrac_a = 1,
-                           prior_sampfrac_b = 19,
-                           start_off_p = .5,
-                           start_neg = 1.48,
-                           start_pi = .5,
-                           start_off_r = 1,
-                           share = c("neg", "off.r"),
-                           update_neg = TRUE,
-                           update_off_r = TRUE,
-                           update_off_p = FALSE,
-                           update_pi = TRUE,
-                           optiStart = 2,
-                           verbose = FALSE,
-                           delta_t = 0.01,
-                           mcmc_iterations = 100000,
-                           thinning = 10) {
-  # Force no sharing for trees_sample
-  if (type == "trees_sample") share <- NULL
+                           ...) {
+  # Some TP arguments are set in this function - ignore
+  # if they are passed in
+  if ("ptree_lst" %in% names(list(...))) {
+    warning("Ignoring user-provided input ptree_list, which is set in this function.")
+  }
+  if ("dateT" %in% names(list(...))) {
+    warning("Ignoring user-provided input dateT, which is set in this function.")
+  }
 
   # Ensure names of trees are in provided cluster dictionary
   # (Not all clusters in the dictionary need a tree, but all
@@ -116,49 +76,34 @@ run_TransPhylo <- function(trees,
     # Reorder dates last sample to match tree order
     dates_last_sample <- dates_last_sample[match(names(trees),
                                                  dates_last_sample$cluster_name),
-                                          "collectdt"]
+                                           "collectdt"]
     # Convert ape phylo trees to TP ptrees using date_last_sample
     ptree_list <- mapply(FUN = function(tree, date_last_sample) {
       TransPhylo::ptreeFromPhylo(tree, date_last_sample)
     }, trees, dates_last_sample, SIMPLIFY = FALSE)
+    # Get the latest date for the TransPhylo run
+    date_last_sample <- max(dates_last_sample)
   } else if (type == "trees_sample") {
     # Pull the correct cluster from the cluster dictionary
     # & get date of last sample
-    cluster_dict <- cluster_dict[cluster_dict$cluster_name == cluster_name, ]
+    date_last_sample <- cluster_dict$collectdt[cluster_dict$cluster_name == cluster_name, ]
     # Convert to ptrees
     ptree_list <- lapply(trees, FUN = function(tree) {
       TransPhylo::ptreeFromPhylo(tree, date_last_sample)
     })
   }
-  # Get the latest date from the cluster dictionary for the
-  # TransPhylo run
-  date_last_sample <- max(cluster_dict$collectdt)
+  # Force no sharing for trees_sample
+  args <- list(...)
+  if (type == "trees_sample") {
+    args$share <- NULL
+  }
+  args$ptree_lst <- ptree_list
+  args$dateT <- date_last_sample + 0.01
   # Run TransPhylo multitree
-  tp_res <- TransPhylo::infer_multittree_share_param(ptree_list,
-                                                     mcmcIterations = mcmc_iterations,
-                                                     thinning = thinning,
-                                                     w.shape = gentime_shape,
-                                                     w.scale = gentime_scale,
-                                                     ws.shape = sampling_shape,
-                                                     ws.scale = sampling_scale,
-                                                     startOff.r = start_off_r,
-                                                     startOff.p = start_off_p,
-                                                     startNeg = start_neg,
-                                                     startPi = start_pi,
-                                                     prior_pi_a = prior_sampfrac_a,
-                                                     prior_pi_b = prior_sampfrac_b,
-                                                     updateNeg = update_neg,
-                                                     updateOff.r = update_off_r,
-                                                     updateOff.p = update_off_p,
-                                                     updatePi = update_pi,
-                                                     share = share,
-                                                     optiStart = optiStart,
-                                                     delta_t,
-                                                     dateT = date_last_sample + .01,
-                                                     verbose = verbose)
+  tp_res <- do.call(TransPhylo::infer_multittree_share_param, args)
 
   # Save resTransPhylo object(s)
-  if(!dir.exists(out_dir)) dir.create(out_dir)
+  if (!dir.exists(out_dir)) dir.create(out_dir)
   saveRDS(tp_res, file = file.path(out_dir,
                                    paste0(output_name, ".rds")))
 
