c9854ca3e12dbe7046497bd2a657a7056510ce24
Jessalyn Sebastian <jessalys@uci.edu>
2024-12-19 17:06:51 -0800

    started adding misclass logistic regression w stan

--- R/regression.R
+++ R/regression.R
@@ -6,7 +6,7 @@
 #' to label an individual as an infection source is specified.
 #' 
 #' @param method A character string specifying the type of regression to run.
-#' Can be "linear" or "logistic".
+#' Can be "linear", "logistic", or "bayesian_logistic_misclass".
 #' @param cleaned_data A data frame containing the covariates to use in the
 #' regression. Must contain a column "SampleID" with the sample IDs.
 #' @param prob_source A data frame of probabilities output by TransPhylo with columns
@@ -14,18 +14,30 @@
 #' the probability of each sample being the infection source.
 #' @param prob_cutoff A numeric specifying the probability cutoff to use for
 #' logistic regression. If NULL, logistic regression will not be run.
+#' @param sensitivity Numeric. Only used if `method` is "bayesian_logistic_misclass".
+#' @param specificity Numeric. Only used if `method` is "bayesian_logistic_misclass".
+#' @param ... Additional arguments to pass to Stan if `method` is
+#' "bayesian_logistic_misclass". Iterations and chains default to 2000 and 4
+#' respectively.
 #'
-#' @returns An `lm` or `glm` object.
+#' @returns An `lm` object, a `glm` object, or a `stanfit`.
+#' 
+#' @importFrom rstan stan extract
 #'
 #' @export
 
-regression <- function(method = c("logistic", "linear"), cleaned_data,
-                       prob_source, prob_cutoff = NULL) {
+regression <- function(method = c("logistic", "linear",
+                                  "bayesian_logistic_misclass"),
+                       cleaned_data, prob_source, prob_cutoff = NULL,
+                       sensitivity = NULL, specificity = NULL,
+                       ...) {
   # We will assume cleaned_data is a data frame with only the
   # covariates we want to use in the regression, and that the
   # sample ids are given in the column "SampleID"
   cleaned_data <- merge(cleaned_data, prob_source, by = "SampleID")
   cleaned_data <- cleaned_data[, !names(cleaned_data) %in% "SampleID"]
+  # Drop rows with NA values
+  cleaned_data <- cleaned_data[complete.cases(cleaned_data), ]
   if (method == "linear") {
     return(lm(prob_source ~ ., data = cleaned_data))
   }
@@ -33,7 +45,43 @@ regression <- function(method = c("logistic", "linear"), cleaned_data,
     if (is.null(prob_cutoff)) {
       stop("Must provide a probability cutoff for logistic regression")
     }
-    cleaned_data$prob_source <- as.factor(cleaned_data$prob_source > prob_cutoff)
-    return(glm(prob_source ~ ., data = cleaned_data, family = "binomial"))
+    cleaned_data$tp_source <- as.factor(cleaned_data$prob_source > prob_cutoff)
+    return(glm(tp_source ~ ., data = cleaned_data, family = "binomial"))
+  }
+  if (method == "bayesian_logistic_misclass") {
+    if (is.null(prob_cutoff)) {
+      stop("Must provide a probability cutoff for logistic regression")
+    }
+    if (is.null(sensitivity) || is.null(specificity)) {
+      stop("Must provide sensitivity and specificity for Bayesian logistic regression")
+    }
+    # check that all columns are numeric
+    if (!all(sapply(cleaned_data, is.numeric))) {
+      stop("All columns in cleaned_data must be numeric for Bayesian logistic regression")
+    }
+    cleaned_data$tp_source <- as.numeric(cleaned_data$prob_source > prob_cutoff)
+    covariates <- cleaned_data[, !(names(cleaned_data) %in% c("prob_source", "tp_source"))]
+    # Run Isaac's Bayesian "homebrew": logistic regression with misclassification
+    model_objects <- list(N = nrow(cleaned_data),
+                          K = ncol(cleaned_data) - 2,
+                          z = cleaned_data$tp_source,
+                          X = covariates,
+                          specificity = specificity,
+                          sensitivity = sensitivity)
+    stanfile <- system.file("Stan", "logistic_misclass.stan",
+                            package = "beast2tpPipeline")
+    args <- list(...)
+    if (!"iter" %in% names(args)) {
+      args$iter <- 2000
+    }
+    if (!"chains" %in% names(args)) {
+      args$chains <- 4
+    }
+    args$file <- stanfile
+    args$data <- model_objects
+    stanfit <- do.call(rstan::stan, args)
+    samples <- rstan::extract(stanfit)
+    colnames(samples$beta) <- colnames(covariates)
+    return(samples)
   }
 }
---
+++ inst/Stan/logistic_misclass.stan
@@ -0,0 +1,45 @@
+// This model is adapted from Isaac Goldstein, see:
+// https://github.com/igoldsteinh/kopanyo_tp_code/blob/main/R/misclass_logistic_regression.stan
+
+//This model is for logistic regression with misclassified responses on some covariates
+//here z is the observed response, possibly misclassified, y is the true response
+//we assume there is no dependence between misclassification and the covariate
+//we assume that sensitivity and specificity of misclassification are known
+data {
+  //number of observations
+  int<lower=0> N;
+  //number of covariates
+  int<lower=0> K;
+  //misclassified response
+  int<lower=0, upper=1> z[N];
+  //covariates
+  matrix[N,K] X;
+  //prob(z=0 | y=0)
+  real<lower=0> specificity;
+  //prob(z = 1 | y=1)
+  real<lower=0> sensitivity;
+}
+
+parameters {
+  real beta0;
+  vector[K] beta;
+}
+
+model {
+  vector[N]  prob_y1;
+  vector[N]  prob_y0;
+  vector[N]  p;
+  
+  // Priors on coefficients - std normal
+  beta0 ~ std_normal();
+  beta ~ std_normal();
+  
+  for (n in 1:N) {
+    prob_y1[n] = inv_logit(beta0 + dot_product(beta, X[n,]));
+    prob_y0[n] = 1-prob_y1[n];
+    p[n] = sensitivity*prob_y1[n] + (1-specificity)*prob_y0[n];
+
+  }
+
+  z ~ bernoulli(p);
+}
